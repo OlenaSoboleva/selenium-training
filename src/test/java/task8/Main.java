e3b2b49b8e2d31027947645302f2112f117eee73
Alina <selena.soboleva@gmail.com>
2017-03-24 22:48:52 +0200

    changed assert

--- src/test/java/task8/Main.java
+++ src/test/java/task8/Main.java
@@ -4,29 +4,31 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
 
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 public class Main extends BasePage {
 @Test
-public void test()throws Throwable {
-    LoginTest();
-    ListDuggs();
+public void stickerTest()throws Throwable {
+    loginTest();
+    listDuggs();
 }
 
-    public void LoginTest() throws Throwable {
+    public void loginTest() throws Throwable {
         driver.navigate().to("http://localhost/litecart/en/");
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("nav#site-menu")));
         assertTrue(isElementPresent(driver, By.cssSelector("nav#site-menu")));
     }
 
-    public void ListDuggs() throws Throwable {
+    public void listDuggs() throws Throwable {
         List<WebElement> CatalogList = driver.findElements(By.cssSelector("div.middle [class=link]"));
         for(int i = 0 ; i < CatalogList.size(); i++){
-            Assert.assertEquals(1, CatalogList.get(i).findElements(By.cssSelector("div > div[class*=sticker]")).size());
+            assertNotNull(CatalogList.get(i).findElement(By.cssSelector("div > div[class*=sticker]")));
         }
     }
 }
